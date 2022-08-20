7739c906ce1cce51f16d2ed7a5ba4bf25244a02a
SabbirSami <smd71430@gmail.com>
2022-08-20 12:15:46 +0600

    create todolist and set design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import Header from "./Components/Header";
 import Navbar from "./Components/Navbar";
+import TodoList from "./Components/TodoList";
 
 function App() {
     return (
         <div>
-            <Navbar />
-            <Header />
+            <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
+                <Navbar />
+
+                <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
+                    <Header />
+
+                    <hr className="mt-4" />
+
+                    <TodoList />
+
+                    <hr className="mt-4" />
+
+                    {/* <Footer /> */}
+                </div>
+            </div>
         </div>
     );
 }
