14b5a0eac4ad71dcedc31664f4a5e310ca51aae2
SabbirSami <smd71430@gmail.com>
2022-08-23 10:24:37 +0600

    added todo reducer

---
+++ src/Redux/todos/reducer.js
@@ -0,0 +1,64 @@
+import { initialState } from "./initialState";
+import {
+    ADDED,
+    ALLCOMPLETED,
+    CLEARCOMPLETED,
+    COLORSELECTED,
+    DELETED,
+    TOGGLED,
+} from "./actionTypes";
+
+const nextTodoId = (todos) => {
+    const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), 0);
+    return maxId + 1;
+};
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADDED:
+            return [
+                ...state,
+                {
+                    id: nextTodoId(state),
+                },
+            ];
+        case TOGGLED:
+            return state.map((todo) => {
+                if (todo.id !== action.payload) {
+                    return todo;
+                } else {
+                    return {
+                        ...state,
+                        completed: !todo.completed,
+                    };
+                }
+            });
+        case COLORSELECTED:
+            return state.map((todo) => {
+                const { todoId, color } = action;
+                if (todo.id !== action.payload) {
+                    return todo;
+                } else {
+                    return {
+                        ...todo,
+                        color: !color,
+                    };
+                }
+            });
+        case DELETED:
+            return state.filter((todo) => todo.id !== action.payload);
+        case ALLCOMPLETED:
+            return state.map((todo) => {
+                return {
+                    ...todo,
+                    completed: true,
+                };
+            });
+        case CLEARCOMPLETED:
+            return state.filter((todo) => !todo.completed);
+
+        default:
+            break;
+    }
+};
+
+export default reducer;
