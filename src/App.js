e7bf81c22192cb9ebaa18f3ee3cea19c85806a79
SabbirSami <smd71430@gmail.com>
2022-08-23 10:56:27 +0600

    added provider

--- src/App.js
+++ src/App.js
@@ -2,26 +2,30 @@ import Header from "./Components/Header";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import TodoList from "./Components/TodoList";
+import { Provider } from "react-redux";
+import store from "./Redux/store";
 
 function App() {
     return (
-        <div>
-            <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
-                <Navbar />
+        <Provider store={store}>
+            <div>
+                <div className="grid place-items-center bg-blue-100 h-screen px-6 font-sans">
+                    <Navbar />
 
-                <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
-                    <Header />
+                    <div className="w-full max-w-3xl shadow-lg rounded-lg p-6 bg-white">
+                        <Header />
 
-                    <hr className="mt-4" />
+                        <hr className="mt-4" />
 
-                    <TodoList />
+                        <TodoList />
 
-                    <hr className="mt-4" />
+                        <hr className="mt-4" />
 
-                    <Footer />
+                        <Footer />
+                    </div>
                 </div>
             </div>
-        </div>
+        </Provider>
     );
 }
 
